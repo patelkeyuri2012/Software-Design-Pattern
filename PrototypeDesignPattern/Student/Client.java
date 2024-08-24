5e5431f4f0135c24f2912e9e18abe0528d7e43f1
Keyuri Patel <patelkeyuri2002@gmail.com>
2024-08-24 09:19:09 +0530

    PrototypeDesignPattern

--- PrototypeDesignPattern/Student/Client.java
+++ PrototypeDesignPattern/Student/Client.java
@@ -2,7 +2,7 @@ public class Client {
     public static void main(String args[]) {
         StudentDetails std = new StudentDetails(20, 7, "Keyuri");
 
-      
+        StudentDetails stdclone = new StudentDetails();
         stdclone.age = std.age;
         stdclone.rollNo = std.rollNo;
         stdclone.name = std.name;
